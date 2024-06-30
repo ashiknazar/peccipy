f3ecbb15020970e4a242a4bd3efbe9c2336b8e9d
ashiknazar <warticcotaky@gmail.com>
2024-06-30 16:00:30 +0530

    changed setup

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+with open("README.md","r") as f:
+    description=f.read()
 setup(
     name='peccipy',
     version='0.0.1', 
@@ -16,7 +18,7 @@ setup(
     author='Ashik Nazar',
     author_email='datas293@gmail.com',
     description='A package for data preprocessing tasks.',
-    long_description=open('README.md').read(),
+    long_description=description,
     long_description_content_type='text/markdown',
     url='https://github.com/ashiknazar/peccipy',  
 )
